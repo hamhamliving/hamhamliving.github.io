3b6b2c5f846151ccf1adc9ffdaca69849d170ada
hamham <lsubig3@gmail.com>
2025-12-31 17:41:24 +0900

    햄햄리빙 꿀템 소

---
+++ data.js
@@ -0,0 +1,47 @@
+const productData = [
+    
+    {
+        "id": "001",
+        "title": "낭만 한도초과 - 노래 나오는 주문제작 마그넷",
+        "imageUrl": "https://t3c.coupangcdn.com/thumbnails/remote/212x212ex/image/vendor_inventory/image_audit/stage/manual/e862f08d14f986206315362f071248b67fb92770c183da2ccecff74301fa_1760414690116.jpg",
+        "link": "https://link.coupang.com/a/djEsEP",
+        "isRocket": true
+    },
+        {
+        "id": "002",
+        "title": "방귀방귀조아",
+        "imageUrl": "https://t3c.coupangcdn.com/thumbnails/remote/212x212ex/image/vendor_inventory/image_audit/stage/manual/e862f08d14f986206315362f071248b67fb92770c183da2ccecff74301fa_1760414690116.jpg",
+        "link": "https://link.coupang.com/a/djEsEP",
+        "isRocket": true
+    },
+            {
+        "id": "003",
+        "title": "test23112321",
+        "imageUrl": "https://t3c.coupangcdn.com/thumbnails/remote/212x212ex/image/vendor_inventory/image_audit/stage/manual/e862f08d14f986206315362f071248b67fb92770c183da2ccecff74301fa_1760414690116.jpg",
+        "link": "https://link.coupang.com/a/djEsEP",
+        "isRocket": true
+    },
+            {
+        "id": "004",
+        "title": "test3",
+        "imageUrl": "https://t3c.coupangcdn.com/thumbnails/remote/212x212ex/image/vendor_inventory/image_audit/stage/manual/e862f08d14f986206315362f071248b67fb92770c183da2ccecff74301fa_1760414690116.jpg",
+        "link": "https://link.coupang.com/a/djEsEP",
+        "isRocket": true
+    },
+            {
+        "id": "005",
+        "title": "test2",
+        "imageUrl": "https://t3c.coupangcdn.com/thumbnails/remote/212x212ex/image/vendor_inventory/image_audit/stage/manual/e862f08d14f986206315362f071248b67fb92770c183da2ccecff74301fa_1760414690116.jpg",
+        "link": "https://link.coupang.com/a/djEsEP",
+        "isRocket": true
+    },
+            {
+        "id": "test",
+        "title": "tes123t11",
+        "imageUrl": "https://t3c.coupangcdn.com/thumbnails/remote/212x212ex/image/vendor_inventory/image_audit/stage/manual/e862f08d14f986206315362f071248b67fb92770c183da2ccecff74301fa_1760414690116.jpg",
+        "link": "https://link.coupang.com/a/djEsEP",
+        "isRocket": true
+    }
+
+    
+];
